fbde3b3b3b8ff39c6b6ddf424e85383657e2d329
NGG-kang <skarndrkd1222@gmail.com>
2021-07-14 23:57:59 +0900

    Update forms.py

--- search_project/search_app/forms.py
+++ search_project/search_app/forms.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
-from .models import MyEnterprise
+from .models import MyEnterprise, SaraminInfo, JobKoreaInfo, JobPlanetInfo, KreditJobInfo, CrwalingPhotos
 from bootstrap_modal_forms.forms import BSModalModelForm
 from django.forms import forms
 from django.shortcuts import get_object_or_404
@@ -9,15 +9,46 @@ from django.shortcuts import get_object_or_404
 class MyEnterpriseForm(BSModalModelForm):
     class Meta:
         model = MyEnterprise
-        exclude = ['author',]
+        exclude = ['author', ]
 
     def clean_company(self, *args, **kwargs):
         company = self.cleaned_data.get('company')
         try:
             # object = get_object_or_404(MyEnterprise, author=self.request.user.pk, company=company)
-            MyEnterprise.objects.filter(author=self.request.user.pk, company=company)
-            raise forms.ValidationError(company + '가 이미 존재합니다')
+            object = MyEnterprise.objects.filter(author=self.request.user.pk, company=company)
+            if object:
+                raise forms.ValidationError(company + '가 이미 존재합니다')
+            return company
         except Exception as e:
             print(e)
             return company
 
+
+class SaraminForm(BSModalModelForm):
+    class Meta:
+        model = SaraminInfo
+        fields = ('__all__')
+
+
+class JobKoreaForm(BSModalModelForm):
+    class Meta:
+        model = JobKoreaInfo
+        fields = ('__all__')
+
+
+class JobPlanetForm(BSModalModelForm):
+    class Meta:
+        model = JobPlanetInfo
+        fields = ('__all__')
+
+
+class KreditJobForm(BSModalModelForm):
+    class Meta:
+        model = KreditJobInfo
+        fields = ('__all__')
+
+
+class CrwalingPhotos(BSModalModelForm):
+    class Meta:
+        model = CrwalingPhotos
+        fields = ('__all__')
