1634fad1582f4a46536ccd862d0987fbe322c79b
NGG-kang <skarndrkd1222@gmail.com>
2021-07-21 09:43:43 +0900

    Update celery.py

--- search_project/search_project/celery.py
+++ search_project/search_project/celery.py
@@ -1,33 +1,36 @@
 import os
 
 from celery import Celery
+from celery_once import QueueOnce
+from django.core.cache import cache
+from celery.schedules import crontab
 
-# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_project.settings')
 
 app = Celery('search_project',
-             broker='redis://localhost:6379/0',
-             backend='redis://localhost:6379/0',
              include=['search_app.tasks'])
-
 app.conf.update(
     result_expired=3600,
 )
 app.conf.timezone = 'Asia/Seoul'
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+# @app.task(bind=True, base=QueueOnce)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
 
-@app.task(bind=True)
-def debug_task(self):
-    i = app.control.inspect()
-    i.scheduled()
-    i.active()
-    i.reserved()
-    print(f'Request: {self.request!r}')
+@app.task
+def today_request_delete():
+    count = cache.get('today_request')
+    cache.decr('today_request', count)
+    # try:
+    #     print('캐시 삭제: ', cache.delete('today_request'))
+    #     print('캐시 생성: ', cache.set('today_request', 0))
+    # except:
+    #     return False
+
+
+@app.task
+def say_hello():
+    print("hello~~")
