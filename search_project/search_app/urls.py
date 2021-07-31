04736631c0c4c8b1d30181d0bd9d292bd9ee9595
NGG-kang <skarndrkd1222@gmail.com>
2021-07-31 18:18:30 +0900

    Update urls.py

--- search_project/search_app/urls.py
+++ search_project/search_app/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from search_app import views
 from rest_framework.routers import DefaultRouter
-
+from django.views.decorators.cache import cache_page
 router = DefaultRouter()
 router.register(r'post', views.PostView)
 router.register(r'comment', views.CommentView)
@@ -15,12 +15,14 @@ urlpatterns = [
     # path('search_job/', views.search_jop_view, name="search_job"),
     path('search_job/', views.SearchJobTemplateView.as_view(), name="search_job"),
     path('search_job_context/', views.SearchBodyTemplateView.as_view(), name="search_job_context"),
-    path('search_company/',views.SearchCompanyBodyListView.as_view(), name="search_company"),
-    path('search_company_context/',
-         views.SearchCompanyBodyListView.as_view(), name="search_company_context"),
+    path('search_company/', views.SearchCompanyBodyListView.as_view(), name="search_company"),
+    path('search_company_context/', views.SearchCompanyBodyListView.as_view(), name="search_company_context"),
     path('task_list/', views.TaskTemplateView.as_view(), name="task_list"),
     path('is_company/', views.is_company, name='is_company'),
 
+
+    path('search_job/users_enters_info/', views.GetUsersEntersInfo.as_view(), name='users_enters_info'),
+    path('search_job/<int:pk>/user_enter_info/', views.GetUserEnterInfo.as_view(), name='user_enter_info'),
     path('create_enter/', views.EnterCreateView.as_view(), name="create_enter"),
     path('update_enter/<int:pk>/', views.EnterUpdateView.as_view(), name='update_enter'),
     path('read_enter/<int:pk>/', views.EnterReadView.as_view(), name='read_enter'),
@@ -35,9 +37,7 @@ urlpatterns = [
     path('crwaling_info_jobkorea/<int:pk>/', views.JobkoreaModalView.as_view(), name='crwaling_info_jobkorea'),
     path('crwaling_info_jobplanet/<int:pk>/', views.JobplanetModalView.as_view(), name='crwaling_info_jobplanet'),
     path('crwaling_info_kreditjob/<int:pk>/', views.KreditJobModalView.as_view(), name='crwaling_info_kreditjob'),
-
-    path('search_job/users_enters_info/', views.GetUsersEntersInfo.as_view(), name='users_enters_info'),
-    path('search_job/<int:pk>/user_enter_info/', views.GetUserEnterInfo.as_view(), name='user_enter_info'),
+    
 ]
 
 urlpatterns += router.urls
